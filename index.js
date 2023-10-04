cca1645410a44f71c0117ca93f789a4cc527956d
Kushalghosh1 <cse.kushal1@gmail.com>
2023-10-04 20:08:38 +0600

    card section added

--- index.js
+++ index.js
@@ -14,10 +14,61 @@ const handleCategory = async () => {
     console.log(data.data.news_category);
 };
 
-const handleLoadNews= async (categoryId)=>{
+const handleLoadNews = async (categoryId) => {
     const response = await fetch(`https://openapi.programming-hero.com/api/news/category/${categoryId}`);
     const data = await response.json();
-    console.log(data);
+
+    const cardContainer = document.getElementById('card-container');
+    data.data.forEach((news) => {
+        console.log(news);
+        const div = document.createElement('div');
+        div.innerHTML = `
+        <div class="card w-96 bg-base-100">
+            <figure>
+                <img src=${news.image_url}>
+            </figure>
+            <div class="card-body">
+                <h2 class="card-title">
+                    Biden Pledges Nearly $3 Billion to Ukraine
+                    <div class="badge badge-secondary p-5">Excellent</div>
+                </h2>
+                <p>
+                    wednesday, August 24, 2022. Tag Cloud tags: Biden,
+                    EU, EURO,
+                    Europe
+                </p>
+                <div class="card-footer flex justify-between mt-8">
+                    <div class="flex">
+                        <div>
+                            <div class="avartar-online">
+                                <div class="w-14 rounded-full">
+                                    <img src="" alt="">
+                                </div>
+                            </div>
+                        </div>
+                        <div>
+                            <h6>Jimmy Dane</h6>
+                            <small>2023-09-25 14:25:33</small>
+                        </div>
+                    </div>
+                    <div class="card-detaild-btn">
+                        <button
+                            class="inline-block cursor-pointer rounded-md bg-gray-800 px-4 py-3 text-center
+                        text-sm font-semibold uppercase text-white transition duration-200 ease-in-out hover:bg-gray-900">
+                        
+                        Details
+
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </div>
+        `
+        cardContainer.appendChild(div);
+
+
+
+    });
 };
 
 handleCategory();
