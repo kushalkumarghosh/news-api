ee89a2216971799356570b064e65f98ef318c971
Kushalghosh1 <cse.kushal1@gmail.com>
2023-10-04 10:45:59 +0600

    handleCategory added

---
+++ index.js
@@ -0,0 +1,17 @@
+const handleCategory = async () => {
+    const response = await fetch("https://openapi.programming-hero.com/api/news/categories");
+    const data = await response.json();
+
+    const tabContainer = document.getElementById("tab-container");
+    data.data.news_category.slice(0, 3).forEach((category) => {
+        // console.log(category);
+        const div = document.createElement("div");
+        div.innerHTML = `
+        <a class="tab">${category.category_name}</a>
+        `;
+        tabContainer.appendChild(div);
+    });
+    console.log(data.data.news_category);
+};
+
+handleCategory();
