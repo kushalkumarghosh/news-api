8fd1e2c080678361b4280f2005add5585f3fef5d
Kushalghosh1 <cse.kushal1@gmail.com>
2023-10-04 11:10:01 +0600

    handleLoadNews() added

--- index.js
+++ index.js
@@ -7,11 +7,17 @@ const handleCategory = async () => {
         // console.log(category);
         const div = document.createElement("div");
         div.innerHTML = `
-        <a class="tab">${category.category_name}</a>
+        <a onclick="handleLoadNews('${category.category_id}')" class="tab">${category.category_name}</a>
         `;
         tabContainer.appendChild(div);
     });
     console.log(data.data.news_category);
+};
+
+const handleLoadNews= async (categoryId)=>{
+    const response = await fetch(`https://openapi.programming-hero.com/api/news/category/${categoryId}`);
+    const data = await response.json();
+    console.log(data);
 };
 
 handleCategory();
